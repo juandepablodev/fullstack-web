b1c636bb3ad8118acd10c36581d1b3dc555b5dc4
Juandepablodev <juandepablo.dev@gmail.com>
2026-06-29 15:15:57 +0200

    Lab 6: Peticiones AJAX

--- web/js/photo_detail.js
+++ web/js/photo_detail.js
@@ -1,28 +1,32 @@
 "use strict";
 
 import { photoRenderer } from "/js/renderers/photos.js";
+import { photosAPI_auto } from "/js/api/_photos.js";
+import { messageRenderer } from "/js/renderers/messages.js";
 
-function main() {
-    let photoContainer = document.querySelector("#photo-details-column");
+// Get the ID of the photo to load from the URL params
+let urlParams = new URLSearchParams(window.location.search);
+let photoId = urlParams.get("photoId");
 
-    let photo = {
-        title: "Samoyed",
-        description: "A very good boy.",
-        userId: 1,
-        url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
-        date: "12/01/1996",
-    };
+async function main() {
+    // Check that we have an ID before doing anything else
+    if (photoId === null) {
+        messageRenderer.showErrorMessage("Please, provide a photoId");
+        return;
+    }
 
-    let photoDetails = photoRenderer.asDetails(photo);
-    photoContainer.appendChild(photoDetails);
-
-    hideActionsColumn();
+    loadPhotoDetails();
 }
 
-function hideActionsColumn() {
-    let actions_col = document.getElementById("actions-col");
-    if (!sessionManager.isLogged()) {
-        actions_col.style.display = "none";
+async function loadPhotoDetails() {
+    let photoContainer = document.querySelector("#photo-details-column");
+    try {
+        let photo = await photosAPI_auto.getById(photoId)
+        let photoDetails = photoRenderer.asDetails(photo);
+        photoContainer.appendChild(photoDetails);
+    } catch (err) {
+        messageRenderer.showErrorMessage("Error loading photo", err);
+        
     }
 }
 
--- web/js/renderers/photos.js
+++ web/js/renderers/photos.js
@@ -3,37 +3,43 @@
 import { parseHTML } from '../utils/parseHTML.js';
 
 const photoRenderer = {
-    asCard: function(photo) {
-        let html = `<div class="col-md-4">
-      <div class="card bg-dark text-light">
-        <img src="${photo.url}" class="card-img-top" alt="${photo.title}">
+    asCard: function (photo) {
+    let html = `<div class="col-md-4">
+        <div class="card bg-dark text-light">
+            <a href="photo_detail.html?photoId=${photo.photoId}">
+                <img src="${photo.url}" class="card-img-top">
+            </a>
 
-        <div class="card-body">
-          <h5 class="card-title text-center">${photo.title}</h5>
-          <p class="card-text">${photo.description}</p>
-          <p class="text-end">User ${photo.userId}</p>
+            <div class="card-body">
+                <h5 class="card-title text-center">${photo.title}</h5>
+                <p class="card-text">${photo.description}</p>
+                <p class="text-end">
+                    @${photo.username}
+                    <img src="${photo.avatarUrl}" class="photo-user-avatar">
+                </p>
+            </div>
         </div>
-      </div>
     </div>`;
 
     let card = parseHTML(html);
     return card;
-    },
-    asDetails: function (photo) {
-        let html = `<div class="photo-details">
-            <h3>${photo.title}</h3>
-            <h6>${photo.description}</h6>
-            <p>Uploaded by <a href="user_profile.html" class="user-link">User ${photo.userId}</a> on ${photo.date}</p>
+},
 
-            <hr>
+  asDetails: function (photo) {
+      let html = `<div class="photo-details">
+          <h3>${photo.title}</h3>
+          <h6>${photo.description}</h6>
+          <p>Uploaded by <a href="user_profile.html" class="user-link">User ${photo.userId}</a> on ${photo.date}</p>
 
-            <img src="${photo.url}" class="img-fluid">
-        </div>`;
-        
-        let photoDetails = parseHTML(html);
-        return photoDetails;
+          <hr>
+
+          <img src="${photo.url}" class="img-fluid">
+      </div>`;
+      
+      let photoDetails = parseHTML(html);
+      return photoDetails;
     },
-};
+  };
 
 export { photoRenderer };
 
